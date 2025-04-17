d4737c2f7ed7a3ce5b902fba90b552b5ee86fc4a
Eli-Gutierrez <185718519+Eli-Gutierrez@users.noreply.github.com>
2025-04-16 23:38:32 -0600

    Crear siguientes pantallas y establacer diseño de header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, useColorScheme } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function Header() {
   const colorScheme = useColorScheme();
@@ -10,12 +11,24 @@ export default function Header() {
       styles.header,
       { backgroundColor: isDark ? '#1c1c1e' : '#e0e0e0' }
     ]}>
+      <FontAwesome
+        name="arrow-circle-left"
+        size={24}
+        color={isDark ? '#fff' : '#000'}
+      />
+      
       <Text style={[
         styles.headerText,
         { color: isDark ? '#fff' : '#000' }
       ]}>
-    
+        Bienvenido a TuApp
       </Text>
+
+      <FontAwesome
+        name="user"
+        size={24}
+        color={isDark ? '#fff' : '#000'}
+      />
     </View>
   );
 }
@@ -26,8 +39,11 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
-    zIndex: 10, // asegura que esté al frente
+    zIndex: 10,
     paddingVertical: 16,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between', // 👈 Distribuye entre los extremos
     alignItems: 'center',
     borderBottomWidth: 1,
     borderBottomColor: '#888',
